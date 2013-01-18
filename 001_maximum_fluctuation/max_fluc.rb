d3e47289feb9dba1acee4e4455046621f7721af3
fxzuz <paul.vlasov@gmail.com>
2013-01-18 20:40:17 +0100

    rename files, remove comments

--- 001_maximum_fluctuation/max_fluc.rb
+++ 001_maximum_fluctuation/max_fluc.rb
@@ -17,27 +17,3 @@ ARGF.each do |line|
     fluc = (max_price - avg) / max_price * 100
     puts fluc
 end
-
-__END__
-#!/usr/env/bin perl
-
-use strict;
-
-while (my $line = <>) {
-
-    chomp $line;
-
-    my @prices    = split ',', $line;
-    my $max_price = 0;
-    my $avg       = 0;
-
-    for my $price (@prices) {
-
-        $max_price = $price if $price > $max_price;
-        $avg      += $price;
-    }
-
-    $avg     = int($avg / scalar @prices);
-    my $fluc = ($max_price - $avg) / $max_price * 100;
-    print $fluc."\n";
-}
