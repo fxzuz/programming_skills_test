f845eed099fd9b73eaa6f007127fc2649c81313e
fxzuz <paul.vlasov@gmail.com>
2013-02-02 17:38:33 +0100

    optimize solution perl

--- 009_password_checker/pass_check.pl
+++ 009_password_checker/pass_check.pl
@@ -2,18 +2,16 @@
 
 use strict;
 
-while (my $str = <>) {
+while (my $pass = <>) {
 
-    chomp $str;
-    printf "%s\n", is_password_valid($str); 
-}
-
-sub is_password_valid {
-    my $pass = shift;
+    chomp $pass;
 
     my $num_letter = $pass =~ tr/[a-zA-Z]{1}//;
     my $num_digits = $pass =~ tr/\d{1}//;
     
-    return 'INVALID' if $num_letter < 4 or $num_digits < 2;
-    return 'VALID'; 
+    my $result = ($num_letter < 4 or $num_digits < 2)
+               ? 'INVALID'
+               : 'VALID';
+
+    printf "%s\n", $result;
 }
