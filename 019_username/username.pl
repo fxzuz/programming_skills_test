b772ea0ccf716c227ba9254236b6608a3f684279
Pavel Vlasov <paul.vlasov@gmail.com>
2014-04-06 22:52:03 +0400

    update

--- 019_username/username.pl
+++ 019_username/username.pl
@@ -4,17 +4,17 @@ use common::sense;
 
 while (<>) {
 
-    chomp $_;
-    my $len = length($_);
+	chomp $_;
+	my $len = length($_);
 
 	my $is_letter = $_ =~ m/[A-Z]{1}/;
 	my $is_digit  = $_ =~ m/\d{1}/;
 	my $is_spec   = $_ =~ m/[@#*=]{1}/;
 	my $is_space  = $_ =~ m/\s/;
 
-    say (
+	say (
 		$is_letter && $is_digit && $is_spec && !$is_space && $len >= 5 && $len <= 10
-    		? 'PASS'
-        	: 'FAIL'
-    );
+			? 'PASS'
+			: 'FAIL'
+	);
 }
