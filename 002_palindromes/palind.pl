5cba1b9bc212277233d715283500dbf6518d7594
Pavel Vlasov <paul.vlasov@gmail.com>
2014-04-06 22:45:14 +0400

    remove smartmatching, because its depricated in new version of Perl

--- 002_palindromes/palind.pl
+++ 002_palindromes/palind.pl
@@ -6,10 +6,17 @@ my @words;
 while (<>) {
     chomp;
 
-    my @chars     = split //, lc $_;
-    my @rev_chars = reverse @chars;
+    my @chars = split //, lc $_;
+	my $half  = int(scalar @chars / 2) - 1;
+	my $is_not_palind;
+	for (0 .. $half) {
+		next if $chars[$_] eq $chars[-1-$_];
 
-    push @words, $_ if @chars ~~ @rev_chars;
+		$is_not_palind = 1;
+		last;
+	}
+
+   	push @words, $_ unless $is_not_palind;
 }
 
 for (sort @words) {
